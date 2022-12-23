e940aba328889534c98493909a3de146f1cab7a2
Srimai46 <Sira4r@gmail.com>
2022-12-23 11:26:43 +0700

    checkstockproduct_

--- Java/src/CheckStockProduct.java
+++ Java/src/CheckStockProduct.java
@@ -21,27 +21,27 @@ public class CheckStockProduct {
 		System.out.println("-------------------------------------------------");
 		System.out.println("List of product in 'LOW' status.");
 		System.out.println("-------------------------------------------------");
-		for(int a =0;a<pd.length;a++) {
-			if(pd[a].getunit()<5) {
-				System.out.println(">> "+pd[a].getId()+" has "+pd[a].getunit()+" Units");
+		for(Product pd_:pd) {
+			if(pd_.getunit()<5) {
+				System.out.println(">> "+pd_.getId()+" has "+pd_.getunit()+" Units");
 			}
 		}
 		
 		System.out.println("-------------------------------------------------");
 		System.out.println("List of product in 'NORMAL' status.");
 		System.out.println("-------------------------------------------------");
-		for(int i =0;i<pd.length;i++) {
-			if(pd[i].getunit()>4 && pd[i].getunit()<51) {
-				System.out.println(">> "+pd[i].getId()+" has "+pd[i].getunit()+" Units");
+		for(Product pd_:pd) {
+			if(pd_.getunit()>4 && pd_.getunit()<51) {
+				System.out.println(">> "+pd_.getId()+" has "+pd_.getunit()+" Units");
 			}
 		}
 		
 		System.out.println("-------------------------------------------------");
 		System.out.println("List of product in 'HIGH' status.");
 		System.out.println("-------------------------------------------------");
-		for(int i =0;i<pd.length;i++) {
-			if(pd[i].getunit()>50) {
-				System.out.println(">> "+pd[i].getId()+" has "+pd[i].getunit()+" Units");
+		for(Product pd_:pd) {
+			if(pd_.getunit()>50) {
+				System.out.println(">> "+pd_.getId()+" has "+pd_.getunit()+" Units");
 			}
 		}
 
